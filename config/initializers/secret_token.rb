3eb8dd1f0371e2000602cab5a59e9ac8398b5708
anandsun <anchorflow@gmail.com>
2012-09-06 13:54:21 -0400

    Revert "Full project tree added"
    This reverts commit 0cc8d126fb7532aee1c0ff7abc2ec69aaebfd7d6.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Demo::Application.config.secret_token = '915d9ad9c54b23f0c0fedfec97145efcced23ebec729803ddea50fa27590c78370ed9bd22597692db3d732fedeadf1a1615bcf0cca6b433b3e522fe6f5305655'
